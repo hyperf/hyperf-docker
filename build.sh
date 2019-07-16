1a4cda4491a785e079dd0cdcdf435c5cb4da149b
李铭昕 <715557344@qq.com>
2019-07-16 18:22:54 +0800

    Added build shell.

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+SWOOLE_V=$1
+SWOOLE_PV=${SWOOLE_V%\.*}
+SWOOLE_PPV=${SWOOLE_V%%\.*}
+
+# Build base.Dockerfile
+# Build php7.2
+docker build ./7.2/alpine/base/ -t hyperf/hyperf:7.2-alpine-base
+# Build php7.3
+docker build ./7.3/alpine/base/ -t hyperf/hyperf:7.3-alpine-base
+
+# Push ...
+
+if  [ "$SWOOLE_V" != "" ] ; then
+    # Build cli.Dockerfile with Swoole Version.
+    # Build php7.2
+    docker build ./7.2/alpine/cli/ --build-arg swoole=$SWOOLE_V -t hyperf/hyperf:7.2-alpine-cli-$SWOOLE_V
+    docker tag hyperf/hyperf:7.2-alpine-cli-$SWOOLE_V hyperf/hyperf:7.2-alpine-cli-$SWOOLE_PV
+    docker tag hyperf/hyperf:7.2-alpine-cli-$SWOOLE_V hyperf/hyperf:7.2-alpine-cli-$SWOOLE_PPV
+    docker tag hyperf/hyperf:7.2-alpine-cli-$SWOOLE_V hyperf/hyperf:7.2-alpine-cli
+
+    # Build latest
+    docker tag hyperf/hyperf:7.2-alpine-cli-$SWOOLE_V hyperf/hyperf:latest
+
+    # Build php7.3
+    docker build ./7.3/alpine/cli/ --build-arg swoole=$SWOOLE_V -t hyperf/hyperf:7.3-alpine-cli-$SWOOLE_V
+    docker tag hyperf/hyperf:7.3-alpine-cli-$SWOOLE_V hyperf/hyperf:7.3-alpine-cli-$SWOOLE_PV
+    docker tag hyperf/hyperf:7.3-alpine-cli-$SWOOLE_V hyperf/hyperf:7.3-alpine-cli-$SWOOLE_PPV
+    docker tag hyperf/hyperf:7.3-alpine-cli-$SWOOLE_V hyperf/hyperf:7.3-alpine-cli
+
+    # Push ...
+
+fi
