2877fa2d03d748691ec7159f257ae8991646b91b
李铭昕 <715557344@qq.com>
2019-12-04 09:40:31 +0800

    Optimized alpine-base

--- build-test.sh
+++ build-test.sh
@@ -5,6 +5,8 @@ TASK=${1}
 
 # build sandbox
 if [[ ${TASK} == "base" ]]; then
-    export PHP_VERSION=7.2 && docker-compose build alpine-v3.9-base
-    export PHP_VERSION=7.3 && docker-compose build alpine-v3.9-base
+    export PHP_VERSION=7.2 && export ALPINE_VERSION=3.9 && docker-compose build alpine-base
+    export PHP_VERSION=7.3 && export ALPINE_VERSION=3.9 && docker-compose build alpine-base
+    export PHP_VERSION=7.4 && export ALPINE_VERSION=3.9 && docker-compose build alpine-base
+    export PHP_VERSION=7.4 && export ALPINE_VERSION=3.10 && docker-compose build alpine-base
 fi
