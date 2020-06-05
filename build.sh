84a0e64fb76d1fff30af90481d39514345b5f36f
李铭昕 <715557344@qq.com>
2020-06-05 15:23:44 +0800

    Added alpine v3.11

--- build.sh
+++ build.sh
@@ -49,9 +49,14 @@ function tag_and_push() {
 # build base image
 if [[ ${TASK} == "base" ]]; then
     export PHP_VERSION=7.2 && export ALPINE_VERSION=3.9 && docker-compose build alpine-base
+
     export PHP_VERSION=7.3 && export ALPINE_VERSION=3.9 && docker-compose build alpine-base
+    export PHP_VERSION=7.3 && export ALPINE_VERSION=3.10 && docker-compose build alpine-base
+    export PHP_VERSION=7.3 && export ALPINE_VERSION=3.11 && docker-compose build alpine-base
+
     export PHP_VERSION=7.4 && export ALPINE_VERSION=3.9 && docker-compose build alpine-base
     export PHP_VERSION=7.4 && export ALPINE_VERSION=3.10 && docker-compose build alpine-base
+    export PHP_VERSION=7.4 && export ALPINE_VERSION=3.11 && docker-compose build alpine-base
 fi
 
 # build swoole image
@@ -59,21 +64,26 @@ if [[ ${TASK} == "cli" ]]; then
     SWOOLE_VERSION=${2}
     export SWOOLE_VERSION=${SWOOLE_VERSION}
     export PHP_VERSION=7.2 && export ALPINE_VERSION=3.9 && docker-compose build alpine-cli
+
     export PHP_VERSION=7.3 && export ALPINE_VERSION=3.9 && docker-compose build alpine-cli
+    export PHP_VERSION=7.3 && export ALPINE_VERSION=3.10 && docker-compose build alpine-cli
+    export PHP_VERSION=7.3 && export ALPINE_VERSION=3.11 && docker-compose build alpine-cli
+
     export PHP_VERSION=7.4 && export ALPINE_VERSION=3.9 && docker-compose build alpine-cli
     export PHP_VERSION=7.4 && export ALPINE_VERSION=3.10 && docker-compose build alpine-cli
+    export PHP_VERSION=7.4 && export ALPINE_VERSION=3.11 && docker-compose build alpine-cli
 fi
 
 if [[ ${TASK} == "publish" ]]; then
     # Push base image
-    TAGS="7.2-alpine-v3.9-base 7.3-alpine-v3.9-base 7.4-alpine-v3.9-base 7.4-alpine-v3.10-base"
+    TAGS="7.2-alpine-v3.9-base 7.3-alpine-v3.9-base 7.3-alpine-v3.10-base 7.3-alpine-v3.11-base 7.4-alpine-v3.9-base 7.4-alpine-v3.10-base 7.4-alpine-v3.11-base"
     for TAG in ${TAGS}; do
         check_or_push $TAG
     done
 
     SWOOLE_VERSION=${2}
     if [[ ${SWOOLE_VERSION} != "" ]]; then
-        TAGS="7.2-alpine-v3.9-cli 7.3-alpine-v3.9-cli 7.4-alpine-v3.9-cli 7.4-alpine-v3.10-cli"
+        TAGS="7.2-alpine-v3.9-cli 7.3-alpine-v3.9-cli 7.3-alpine-v3.10-cli 7.3-alpine-v3.11-cli 7.4-alpine-v3.9-cli 7.4-alpine-v3.10-cli 7.4-alpine-v3.11-cli"
         SWOOLE_VERSION="v${SWOOLE_VERSION}"
         for TAG in ${TAGS}; do
             BASETAG=${TAG}
