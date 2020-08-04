d4580a9b6165a8ae35e77af086a8557cc5eb7c44
李铭昕 <715557344@qq.com>
2020-08-04 09:51:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -87,6 +87,14 @@ if [[ ${TASK} == "cli" ]]; then
     export PHP_VERSION=7.4 && export ALPINE_VERSION=3.11 && docker-compose build alpine-cli
 fi
 
+if [[ ${TASK} == "publish-dev" ]]; then
+    # Push dev image
+    TAGS="7.2-alpine-v3.9-dev 7.3-alpine-v3.9-dev 7.3-alpine-v3.10-dev 7.3-alpine-v3.11-dev 7.4-alpine-v3.9-dev 7.4-alpine-v3.10-dev 7.4-alpine-v3.11-dev"
+    for TAG in ${TAGS}; do
+        check_or_push $TAG
+    done
+fi
+
 if [[ ${TASK} == "publish" ]]; then
     # Push base image
     TAGS="7.2-alpine-v3.9-base 7.3-alpine-v3.9-base 7.3-alpine-v3.10-base 7.3-alpine-v3.11-base 7.4-alpine-v3.9-base 7.4-alpine-v3.10-base 7.4-alpine-v3.11-base"
