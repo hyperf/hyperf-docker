1252993e8ec250c0c412f3fc8346c3a313e0ffa4
李铭昕 <715557344@qq.com>
2021-05-14 14:44:17 +0800

    Added version 8.0 for base image

--- build.sh
+++ build.sh
@@ -32,11 +32,14 @@ if [[ ${TASK} == "build" ]]; then
     export PHP_VERSION=7.4 && export ALPINE_VERSION=3.10 && docker-compose build alpine-base
     export PHP_VERSION=7.4 && export ALPINE_VERSION=3.11 && docker-compose build alpine-base
     export PHP_VERSION=7.4 && export ALPINE_VERSION=3.12 && docker-compose build alpine-base
+
+    export PHP_VERSION=8.0 && export ALPINE_VERSION=3.11 && docker-compose build alpine-base
+    export PHP_VERSION=8.0 && export ALPINE_VERSION=3.12 && docker-compose build alpine-base
 fi
 
 if [[ ${TASK} == "publish" ]]; then
     # Push base image
-    TAGS="7.3-alpine-v3.10-base 7.3-alpine-v3.11-base 7.4-alpine-v3.10-base 7.4-alpine-v3.11-base 7.4-alpine-v3.12-base"
+    TAGS="7.3-alpine-v3.10-base 7.3-alpine-v3.11-base 7.4-alpine-v3.10-base 7.4-alpine-v3.11-base 7.4-alpine-v3.12-base 8.0-alpine-v3.11-base 8.0-alpine-v3.12-base"
     for TAG in ${TAGS}; do
         check_or_push $TAG
     done
