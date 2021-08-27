9d5dde06eb28f46fa3eb1568574145f3fb1f984b
李铭昕 <715557344@qq.com>
2021-08-27 10:48:23 +0800

    Upgrade Swoole version to v4.7.1

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,9 +3,9 @@
 docker-compose build alpine-$ENGINE
 
 # Login
-docker login docker.pkg.github.com -u hyperf -p $GITHUB_TOKEN
+# docker login docker.pkg.github.com -u hyperf -p $GITHUB_TOKEN
 docker login --username limingxinleo -p $DOCKERHUB_TOKEN
 
 # Push to Github
-docker tag hyperf/hyperf:$PHP_VERSION-alpine-v$ALPINE_VERSION-$ENGINE-$SW_VERSION docker.pkg.github.com/hyperf/hyperf-docker/hyperf:$PHP_VERSION-alpine-v$ALPINE_VERSION-$ENGINE-$SW_VERSION
-docker push docker.pkg.github.com/hyperf/hyperf-docker/hyperf:$PHP_VERSION-alpine-v$ALPINE_VERSION-$ENGINE-$SW_VERSION
+# docker tag hyperf/hyperf:$PHP_VERSION-alpine-v$ALPINE_VERSION-$ENGINE-$SW_VERSION docker.pkg.github.com/hyperf/hyperf-docker/hyperf:$PHP_VERSION-alpine-v$ALPINE_VERSION-$ENGINE-$SW_VERSION
+# docker push docker.pkg.github.com/hyperf/hyperf-docker/hyperf:$PHP_VERSION-alpine-v$ALPINE_VERSION-$ENGINE-$SW_VERSION
