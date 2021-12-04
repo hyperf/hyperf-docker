bb039e725ee81e095a67127319a6b3ee68193860
sy-records <52o@qq52o.cn>
2021-12-04 12:04:39 +0800

    Add php 8.1 images

--- build.sh
+++ build.sh
@@ -41,11 +41,17 @@ if [[ ${TASK} == "build" ]]; then
     export PHP_VERSION=8.0 && export ALPINE_VERSION=3.13 && docker-compose build alpine-origin-base
     export PHP_VERSION=8.0 && export ALPINE_VERSION=3.14 && docker-compose build alpine-origin-base
     export PHP_VERSION=8.0 && export ALPINE_VERSION=3.15 && docker-compose build alpine-origin-base
+
+    export PHP_VERSION=8.1 && export ALPINE_VERSION=3.11 && docker-compose build alpine-base
+    export PHP_VERSION=8.1 && export ALPINE_VERSION=3.12 && docker-compose build alpine-base
+    export PHP_VERSION=8.1 && export ALPINE_VERSION=3.13 && docker-compose build alpine-origin-base
+    export PHP_VERSION=8.1 && export ALPINE_VERSION=3.14 && docker-compose build alpine-origin-base
+    export PHP_VERSION=8.1 && export ALPINE_VERSION=3.15 && docker-compose build alpine-origin-base
 fi
 
 if [[ ${TASK} == "publish" ]]; then
     # Push base image
-    TAGS="7.3-alpine-v3.10-base 7.3-alpine-v3.11-base 7.4-alpine-v3.10-base 7.4-alpine-v3.11-base 7.4-alpine-v3.12-base 7.4-alpine-v3.13-base 7.4-alpine-v3.14-base 7.4-alpine-v3.15-base 8.0-alpine-v3.11-base 8.0-alpine-v3.12-base 8.0-alpine-v3.13-base 8.0-alpine-v3.14-base 8.0-alpine-v3.15-base"
+    TAGS="7.3-alpine-v3.10-base 7.3-alpine-v3.11-base 7.4-alpine-v3.10-base 7.4-alpine-v3.11-base 7.4-alpine-v3.12-base 7.4-alpine-v3.13-base 7.4-alpine-v3.14-base 7.4-alpine-v3.15-base 8.0-alpine-v3.11-base 8.0-alpine-v3.12-base 8.0-alpine-v3.13-base 8.0-alpine-v3.14-base 8.0-alpine-v3.15-base 8.1-alpine-v3.11-base 8.1-alpine-v3.12-base 8.1-alpine-v3.13-base 8.1-alpine-v3.14-base 8.1-alpine-v3.15-base"
     for TAG in ${TAGS}; do
         check_or_push $TAG
     done
