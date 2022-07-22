0b44089fbb65e8dba93a9cc5f07ded0e8d2f7fd7
pandaLIU <26201936+PandaLIU-1111@users.noreply.github.com>
2022-07-22 14:16:14 +0800

    Fixed build-ubuntu.sh UBUNTU_VERSION
    ubuntu 20.10 这个版本是不存在的

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -8,12 +8,12 @@ CHECK=${!#}
 
 # build base image
 if [[ ${TASK} == "base" ]]; then
-    export PHP_VERSION=7.4 && export UBUNTU_VERSION=20.10 && docker-compose -f ubuntu.compose.yml build ubuntu-base
+    export PHP_VERSION=7.4 && export UBUNTU_VERSION=21.10 && docker-compose -f ubuntu.compose.yml build ubuntu-base
 fi
 
 # build swoole image
 if [[ ${TASK} == "cli" ]]; then
     SWOOLE_VERSION=${2}
     export SWOOLE_VERSION=${SWOOLE_VERSION}
-    export PHP_VERSION=7.4 && export UBUNTU_VERSION=20.10 && docker-compose -f ubuntu.compose.yml build ubuntu-cli
+    export PHP_VERSION=7.4 && export UBUNTU_VERSION=21.10 && docker-compose -f ubuntu.compose.yml build ubuntu-cli
 fi
