6504b67d37353b9fcb82577f3104fae621559ef4
李铭昕 <715557344@qq.com>
2022-07-26 16:41:48 +0800

    Removed useless shell

--- .github/workflows/push.sh
+++ .github/workflows/push.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Login
+docker login --username limingxinleo -p $DOCKERHUB_TOKEN
+
 # Push to DockerHub
 docker tag hyperf/hyperf:${PHP_VERSION}-alpine-v${ALPINE_VERSION}-${ENGINE}-${SW_VERSION} hyperf/hyperf:${PHP_VERSION}-alpine-v${ALPINE_VERSION}-${ENGINE}-${SW_VERSION%\.*}
 docker tag hyperf/hyperf:${PHP_VERSION}-alpine-v${ALPINE_VERSION}-${ENGINE}-${SW_VERSION} hyperf/hyperf:${PHP_VERSION}-alpine-v${ALPINE_VERSION}-${ENGINE}-${SW_VERSION%%\.*}
